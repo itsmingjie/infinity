ac30e9424d936fc70918813f39b049916ece562f
Mingjie Jiang <jiang@mingjie.info>
2020-10-18 00:41:41 -0400

    Update db generator for display_name

--- sql/db_init.sql
+++ sql/db_init.sql
@@ -1,24 +1,24 @@
-CREATE TABLE teams (
-    id uuid NOT NULL CONSTRAINT users_pkey PRIMARY KEY,
+create table if not exists teams (
+    id uuid not null constraint users_pkey primary key,
     name char(64),
     password varchar(64),
-    "displayName" char(64),
+    display_name char(64),
     admin boolean,
     affiliation char(64),
-    score integer DEFAULT 0 NOT NULL,
-    banned boolean DEFAULT FALSE
+    score integer default 0 not null,
+    banned boolean default false
 );
 
-CREATE UNIQUE INDEX teams_name_uindex ON teams (name);
+create unique index if not exists teams_name_uindex on teams (name);
 
-CREATE TABLE logs (
-    id uuid NOT NULL CONSTRAINT attempts_pk PRIMARY KEY,
-    uid uuid NOT NULL,
-    puzzle text NOT NULL,
-    timestamp timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    attempt text NOT NULL,
-    action text DEFAULT 'solve' ::text NOT NULL,
-    value integer DEFAULT 0
+create table if not exists logs (
+    id uuid not null constraint attempts_pk primary key,
+    uid uuid not null,
+    puzzle text not null,
+    timestamp timestamp default CURRENT_TIMESTAMP not null,
+    attempt text not null,
+    action text default 'solve' :: text not null,
+    value integer default 0
 );
 
-CREATE UNIQUE INDEX attempts_id_uindex ON logs (id);
+create unique index if not exists attempts_id_uindex on logs (id);
