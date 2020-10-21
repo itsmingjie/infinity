d1d11eaacca030dd5936ace4b6cde5e5c423ee40
Mingjie Jiang <jiang@mingjie.dev>
2020-10-21 12:31:16 -0400

    Adapt through redis

--- services/socketio.js
+++ services/socketio.js
@@ -1,20 +1,28 @@
 let io
 
+const redis = require('redis')
+const config = require('../config')
+const redisAdapter = require('socket.io-redis')
+const pub = redis.createClient(config.redis)
+const sub = redis.createClient(config.redis)
+
 const init = (http) => {
   io = require('socket.io')(http)
+  io.adapter(redisAdapter({ pubClient: pub, subClient: sub }))
+
   return io
 }
 
 const interface = () => {
-    if (!io) {
-        throw new Error("IO has not been initialized! Run init() first.")
-    }
+  if (!io) {
+    throw new Error('IO has not been initialized! Run init() first.')
+  }
 
-    return io
+  return io
 }
 
 const numUsersOnline = () => {
-    return io.engine.clientsCount
+  return io.engine.clientsCount
 }
 
-module.exports = {init, interface, numUsersOnline}
+module.exports = { init, interface, numUsersOnline }
