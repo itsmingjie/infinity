8822ff9ed926664b83016f56ea3b36d430ca28fb
Mingjie Jiang <jiang@mingjie.dev>
2020-10-21 12:36:51 -0400

    Use Websocket transport only

--- static/assets/scripts/global.js
+++ static/assets/scripts/global.js
@@ -18,7 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 // socket.io announcements
-const socket = io();
+const socket = io({
+  transports: [ 'websocket' ]
+});
 const statusEl = document.getElementById("socket-status")
 
 socket.on('connect', () => {
