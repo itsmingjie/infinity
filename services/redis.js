2149a766d9384dadb3606ef1e0cc683f88e31414
Mingjie Jiang <jiang@mingjie.dev>
2020-11-03 20:55:39 -0500

    Revert

--- services/redis.js
+++ services/redis.js
@@ -12,7 +12,7 @@ client.on('connect', () => {
 })
 
 // settings cache
-let settings
+let settings = {}
 
 const getSettings = () => {
   return new Promise((resolve, reject) => {
@@ -20,8 +20,7 @@ const getSettings = () => {
       if (err) {
         return reject(err)
       } else if (!reply) {
-        console.error("Something went wrong with Redis.")
-        process.exit(-1)
+        resolve({})
       }
 
       settings = JSON.parse(reply)
