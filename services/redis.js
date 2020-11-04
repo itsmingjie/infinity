855539586f9c4ba478e6348ec1323221a9e1ee09
Mingjie Jiang <jiang@mingjie.dev>
2020-11-03 20:35:47 -0500

    Revert

--- services/redis.js
+++ services/redis.js
@@ -5,8 +5,6 @@ const client = redis.createClient(config.redis)
 
 client.on('error', (error) => {
   console.error(error)
-  console.error("Something went wrong with Redis.")
-  process.exit(-1)
 })
 
 client.on('connect', () => {
