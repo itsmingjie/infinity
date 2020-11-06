40a601e2f63d28f8fd41f2f78f36d46138bcb19d
Mingjie Jiang <jiang@mingjie.dev>
2020-11-06 04:09:15 -0500

    Add confetti

--- static/assets/scripts/solve.js
+++ static/assets/scripts/solve.js
@@ -69,8 +69,18 @@ const solve = (e) => {
           icon: 'success',
           html: res.message,
           title: 'Correct!',
-          footer: '[Reference: ' + res.reference + ' — share this with us if you think this is an error]'
+          footer:
+            '[Reference: ' +
+            res.reference +
+            ' — share this with us if you think this is an error]'
         })
+        
+        if (res.isMeta) {
+          fireworks()
+        } else {
+          fireConfetti()
+        }
+
         document.getElementById(
           'solve-wrapper'
         ).innerHTML = `<p>${res.message}</p>`
@@ -81,14 +91,17 @@ const solve = (e) => {
           icon: 'error',
           html: res.message,
           title: 'Incorrect!',
-          footer: '[Reference: ' + res.reference + ' — share this with us if you think this is an error]',
+          footer:
+            '[Reference: ' +
+            res.reference +
+            ' — share this with us if you think this is an error]',
           showDenyButton: true,
           denyButtonText: 'Stuck?',
           showConfirmButton: true,
           confirmButtonText: 'Retry!'
         }).then((result) => {
           if (result.isDenied) {
-            window.location.href="/page/stuck"
+            window.location.href = '/page/stuck'
           }
         })
         solutionBox.value = ''
@@ -100,3 +113,74 @@ const solve = (e) => {
 }
 
 solveBtn.addEventListener('click', solve)
+
+const fireworks = () => {
+  var duration = 15 * 1000
+  var animationEnd = Date.now() + duration
+  var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 }
+
+  function randomInRange(min, max) {
+    return Math.random() * (max - min) + min
+  }
+
+  var interval = setInterval(function () {
+    var timeLeft = animationEnd - Date.now()
+
+    if (timeLeft <= 0) {
+      return clearInterval(interval)
+    }
+
+    var particleCount = 50 * (timeLeft / duration)
+    // since particles fall down, start a bit higher than random
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 }
+      })
+    )
+    confetti(
+      Object.assign({}, defaults, {
+        particleCount,
+        origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 }
+      })
+    )
+  }, 250)
+}
+
+const fireConfetti = () => {
+  var count = 200
+  var defaults = {
+    origin: { y: 0.7 }
+  }
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio)
+      })
+    )
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55
+  })
+  fire(0.2, {
+    spread: 60
+  })
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  })
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45
+  })
+}
